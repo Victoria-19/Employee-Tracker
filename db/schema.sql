ce8b3c70896d0110886b607ca7938abfd00550da
Victoria Drier <toridrier@yahoo.com>
2023-08-20 23:25:16 -0500

    finished setting up database using Homework structure in module 12

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,26 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department ();
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE role ();
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    Salary DECIMAL(10.2) NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE employee();
+CREATE TABLE employee(
+    id INT NOT Null AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+
+);
