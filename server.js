4083df4866c2f5a8f3c75068dc66780cc15da6c0
Victoria Drier <toridrier@yahoo.com>
2023-08-21 02:23:34 -0500

    Cannot connect to mysql database no matter what I try delted env file and tried to connnect does not work finished the list and added the functions for viewing and adding departments

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const mysql = require('mysql2') //Imports my sql
+ const mysql  = require ('mysql2') //Imports my sql
 
 const inquirer = require('inquirer'); // Imports inquirer
 
 require("console.table"); // Allows you to display data as a table
 
-require('dotenv').config();
+const cTable = require('console.table');
 
-// Mysql connection using .env
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+    host: "localhost",
+     port: "3306",
+     user: "root",
+     password: "employee-tracker",
+    database: "employee_db"
+})
 
 connection.connect(err => {
     if (err) throw err;
     console.log('connected');
-    afterConnection();
+    ;
 });
+
+// Want to try an create Image Like in mockUp
 
 // uses inquirer to prompt user with list 
 const promptUser = () => {
@@ -28,11 +29,111 @@ const promptUser = () => {
         name: 'choices',
         message: 'What would you like to do?',
         choices: [
-            'View all departments',
-            'View all employees',
-            'View all roles',
-            
-
+            'View all Departments',
+            'Add Department',
+            'Delete a Department',
+            'View all Roles',
+            'Add Role',
+            'Delete a role',
+            'View all Employees',
+            'Add Employee',
+            'Delete an Employee',
+            'Update an Employee role',
+            'No Action'
         ]
     }])
-}
+    .then((answers) => {
+        const { choices } = answers;
+
+        if (choices === "View all Departments") {
+            ShowDepartments();
+        }
+
+        if (choices === "Add Department") {
+            AddDepartment();
+        }
+
+        if (choices === "Delete a Department") {
+            DeleteDepartment();
+        }
+
+        if (choices === "View all roles") {
+            ShowAllRoles();
+        }
+
+        if (choices === "Add Role") {
+            AddRole();
+        }
+
+        if (choices === "Delete a role") {
+            DeleteRoll();
+        }
+
+        if (choices === "View all Employees") {
+            ShowAllEmployees();
+        }
+
+        if (choices === "Add Employee") {
+            AddEmployee();
+        }
+
+        if (choices === "Delete an Employee") {
+            DeleteEmployee();
+        }
+
+        if (choices === "Update an Employee role") {
+            UpdateEmployee();
+        }
+
+        if (choices === "No Action") {
+            connection.end();
+        };
+    });  
+};
+
+// Function to View All Departments
+
+ShowDepartments = () => {
+    console.log('Showing All Departments');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+// Function to add a department
+const AddDepartment = () => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'addDept',
+          message: 'What department do you want to add?',
+          validate: (addDept) => {
+            if (addDept) {
+              return true;
+            } else {
+              return 'Please enter a department';
+            }
+          },
+        },
+      ])
+      .then((answer) => {
+        const departmentName = answer.addDept;
+  
+        // SQL query to insert the department into the database
+        const sql = 'INSERT INTO department (name) VALUES (?)';
+        
+        // Execute the query
+        connection.query(sql, [departmentName], (err, result) => {
+          if (err) {
+            console.error('Error adding department:', err);
+          } else {
+            console.log(`Added "${departmentName}" to departments!`);
+          }
+        });
+      });
+  };
