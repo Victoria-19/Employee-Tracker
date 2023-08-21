e493b64f92af6aed10367acae7fca86a493767d9
Victoria Drier <toridrier@yahoo.com>
2023-08-21 00:18:40 -0500

    Worked on seedssql creating roles and departments need to make employees I followed the mock up in module 12 to create them started working on server.js using .env to establish connection to the data base added connection function to server.js as well as starting creating first list prompt for the user to select what they would like to do

--- server.js
+++ server.js
@@ -4,3 +4,35 @@ const inquirer = require('inquirer'); // Imports inquirer
 
 require("console.table"); // Allows you to display data as a table
 
+require('dotenv').config();
+
+// Mysql connection using .env
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected');
+    afterConnection();
+});
+
+// uses inquirer to prompt user with list 
+const promptUser = () => {
+    inquirer.prompt ([{
+        type:'list',
+        name: 'choices',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all employees',
+            'View all roles',
+            
+
+        ]
+    }])
+}
